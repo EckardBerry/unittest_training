92c97c11ab5c45883994dea74c0bc79423b6a154
eckardberry <eckard.berry@gmail.com>
2022-02-04 10:53:46 +0200

    Initial commit

---
+++ adding.py
@@ -0,0 +1,2 @@
+def adding_numbers(num_1, num_2):
+	return (num_1 + num_2)
---
+++ test.py
@@ -0,0 +1,5 @@
+import adding
+
+
+def test_adding_of_numbers_returns_sum_result():
+	assert adding.adding_numbers(2, 3) == 5
